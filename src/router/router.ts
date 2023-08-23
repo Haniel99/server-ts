59b0fa65cb6a58240073ed67cb138698cf2933c0
AnibalN25 <aniballaura25@gmail.com>
2023-08-23 15:48:20 -0400

    Se agrego el vercl.json en la version 7

--- src/router/router.ts
+++ src/router/router.ts
@@ -8,7 +8,8 @@ const router = Router();
  * This function imports the paths of the diferect files and then used 
  * for the router
  * @constant router
- 
+ */
+/*
 fs.readdirSync(path).filter((name)=> {
     const new_name = name.split('.').shift();
     if(new_name != 'router'){
@@ -20,5 +21,17 @@ fs.readdirSync(path).filter((name)=> {
 */
 import("./login").then((new_router) => {
     router.use(`/login`,new_router.router);
+})
+import("./admin").then((new_router) => {
+    router.use(`/admin`,new_router.router);
+})
+import("./professor").then((new_router) => {
+    router.use(`/professor`,new_router.router);
+})
+import("./rector").then((new_router) => {
+    router.use(`/rector`,new_router.router);
+})
+import("./verify").then((new_router) => {
+    router.use(`/verify`,new_router.router);
 })
 export default router; 
